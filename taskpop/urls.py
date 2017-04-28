0de1692757e2a3c11e8309a364047c1bc0a95127
ishaankolluri <ishaankolluri@gmail.com>
2017-04-27 20:43:19 -0400

    config

--- taskpop/urls.py
+++ taskpop/urls.py
@@ -7,6 +7,7 @@ app_name = "taskpop"
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^home$', views.home, name='home'),
+    url(r'^edit$', views.edit, name='edit'),
     url(r'^login$', views.login, name="login"),
     url(r'^logout', views.logout, name="logout"),
 ]
--- taskpop/views.py
+++ taskpop/views.py
@@ -12,11 +12,40 @@ def login(request):
 
 
 def home(request):
-    print request
     return render(request, 'home.html')
 
 
-    # return HttpResponseRedirect(reverse('taskpop:')
+def edit(request):
+    print request.GET
+    # TODO: Request tasks associated with the user key(requests) from Dynamo.
+    # TODO: Build the task list and pass it to edit.html.
+    # TODO: Investigate the format of the task returned to Django from AWS.
+    # Below is a sample task list. NOTE: This could be incorrect JSON.
+    task_one = {
+        "user": "Ishaan",
+        "task_name": "Do the laundry",
+        "due_date": "December 25th, 2017",
+        "deadline": "5:30pm"
+    }
+    task_two = {
+        "user": "Ishaan",
+        "task_name": "Assignment 7",
+        "due_date": "December 25th, 2017",
+        "deadline": "4:30pm"
+    }
+    task_three = {
+        "user": "Ishaan",
+        "task_name": "Assignment 7",
+        "due_date": "December 25th, 2017",
+        "deadline": "4:30pm"
+    }
+    tasks = [task_one, task_two, task_three]
+
+    return render(request, 'edit.html', context={
+            "user": task_one["user"],
+            "tasks": tasks
+    })
+
 
 def logout(request):
     # The OAuth should already have handled this.
