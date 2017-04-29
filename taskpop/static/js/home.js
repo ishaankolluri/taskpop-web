521bef361c6ce318d7e1f56b79bf5822dd9cb528
ishaankolluri <ishaankolluri@gmail.com>
2017-04-29 13:21:54 -0400

    adding edit page

---
+++ taskpop/static/js/home.js
@@ -0,0 +1,10 @@
+$(function(){
+   $("#create").click(function(event){
+      event.preventDefault();
+      var modal = document.getElementById("createModal");
+      modal.style.display = "block";
+      $("#close").click(function(event){
+         modal.style.display = "none";
+      });
+   });
+});
