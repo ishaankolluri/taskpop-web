4f3804b6fd185f5480992e847f246e69db8771dd
Calvin Li <calvinli723@gmail.com>
2017-05-07 18:44:37 -0400

    recollect static

--- static/js/auth.js
+++ static/js/auth.js
@@ -30,3 +30,28 @@ if (cognitoUser != null) {
         console.log(cognitoUser.getUsername());
     });
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        var data = {
+            UserPoolId : 'us-east-1_KZ3VOLM6U',
+            ClientId : '560pmh9r8gmjrp04aijmnf1tmu'
+        };
+        var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(data);
+        var cognitoUser = userPool.getCurrentUser();
+        if(cognitoUser !== null){
+            console.log("Deauthenticating Cognito.");
+            cognitoUser.signOut();
+        }
+        window.location.assign("login");
+
+    });
+}
+
+function onLoad() {
+  gapi.load('auth2', function() {
+    gapi.auth2.init();
+  });
+}
